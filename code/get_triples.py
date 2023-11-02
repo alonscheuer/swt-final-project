c491f1871b3c4020999b08b203e2b6d98f65d61f
Frieso Turkstra <friesoturkstra@gmail.com>
2023-11-02 21:58:03 +0100

    add postprocessing of triples

--- code/get_triples.py
+++ code/get_triples.py
@@ -1,7 +1,6 @@
 import argparse
 from json import load, dump
 from utils import dbp_endpoint, send_query, remove_prefix
-import re
 from tqdm import tqdm
 
 
@@ -10,6 +9,21 @@ def create_arg_parser():
 	parser.add_argument("-i", "--input_file", type=str, help="File containing dbr links")
 	parser.add_argument("-o", "--output_file", type=str, help="Name of file where results are saved")
 	return parser.parse_args()
+
+
+def postprocess(triples):
+	filtered_triples = []
+	for triple in triples:
+		# filter non-English languages 
+		if lang := triple['value'].get('xml:lang'):
+			if lang != 'en':
+				continue
+		# filter certain properties
+		property = remove_prefix(triple['prop']['value'])
+		if property in ['wikiPageUsesTemplate', 'sameAs']:
+			continue
+		filtered_triples.append(clean_triple(triple))
+	return filtered_triples
 
 
 def clean_triple(triple):
@@ -26,18 +40,16 @@ def get_triples(dbr):
 	PREFIX dbr: <http://dbpedia.org/resource/>
 	SELECT ?prop, ?value WHERE {
 		dbr:%s ?prop ?value .
-		FILTER(lang(?value) = "en").
 	}
 	""" % remove_prefix(dbr).replace('(', '\(').replace(')', '\)')
 	result = send_query(query, dbp_endpoint)
-	triples = [clean_triple(t) for t in result]
+	triples = postprocess(result)
 	return triples
 
 
 def main():
 	args = create_arg_parser()
 	items = load(open(args.input_file))
-	items = items[1:5]
 	for item in tqdm(items):
 		item["triples"] = get_triples(item["dbr"])
 	dump(items, open(args.output_file, "w"))
