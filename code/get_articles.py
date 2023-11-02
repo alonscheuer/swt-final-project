0ce377408b8c103dd37d2c4dcae09aaddbdd3728
alonscheuer <11799711+alonscheuer@users.noreply.github.com>
2023-11-02 20:00:46 +0100

    wikipedia extraction

--- code/get_articles.py
+++ code/get_articles.py
@@ -14,21 +14,42 @@ def create_arg_parser():
 
 
 def get_wikipage(wikilink):
-	r = requests.get(wikilink)
-	soup = bs4.BeautifulSoup(r.text,'html.parser').select('body')[0]
-	text = []
-	tags = ['p', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-	for tag in soup.find(id="mw-content-text").descendants:
-		if tag.name in tags:
-			text.append(tag.text)
-	print(text)
-	return " ".join(text)
+	req = requests.get(wikilink)
+	soup = bs4.BeautifulSoup(req.text,'html.parser').select('body')[0]
+	# Get article body
+	main = soup.find(id="mw-content-text")
+	# Get rid of citations in text
+	references = main.find_all("sup", class_="reference")
+	for r in references:
+		r.extract()
+	# Change links into text
+	links = main.find_all("a")
+	for l in links:
+		l.unwrap()
+	# Remove [edit] links from section titles
+	edits = main.find_all(class_="mw-editsection")
+	for e in edits:
+		e.extract()
+	# Remove utility divs
+	divs = main.find_all("div")[1:]
+	for d in divs:
+		d.extract()
+	# Remove unwanted sections
+	discarded_sections = ["See_also", "References", "External_links", "Se_även", "Referenser", "Externa_länkar"]
+	for section in discarded_sections:
+		span = main.find(id=section)
+		if span:
+			h = span.find_parent()
+			rest = h.find_next_siblings()
+			for r in rest:
+				r.extract()
+			h.extract()
+	return main.get_text().strip().replace("\n\n", "\n")
 
 
 def main():
 	args = create_arg_parser()
 	items = load(open(args.input_file))
-	items = items[0:5]
 	for item in tqdm(items):
 		item["en_text"] = get_wikipage(item["en"])
 		item["sv_text"] = get_wikipage(item["sv"])
